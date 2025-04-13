a9026396b2350b39984f8a677853dbea8825d41d
phil-webdev <72519647+phil-webdev@users.noreply.github.com>
2025-04-13 01:54:56 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ========================= Typing animation ========================= */
 var typed = new Typed(".typing",{
-    strings:["","Intégrateur web","Développeur web"],
+    strings:["Développeur web","Intégrateur web","Web Master"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
